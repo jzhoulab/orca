571fbb3360d9e15f045243fc3683bf9f7f55dc3a
Jian Zhou <jzhoup@gmail.com>
2021-03-12 14:15:04 -0600

    add helper scripts

---
+++ misc/make_genome_memmap.py
@@ -0,0 +1,21 @@
+"""
+This script converts the genome fasta file to a memory map
+file of genome one-hot encodings. This is used to accelerate
+genome sequence retrieval (Orca automatically detects the memmap
+file and use it) and is required if you use the training scripts.
+
+Example usage: python make_genome_memmap.py
+"""
+
+import sys
+import pathlib
+
+ORCA_PATH = str(pathlib.Path(__file__).absolute().parent.parent)
+from orca_predict import MemmapGenome
+
+if __name__ == "__init__":
+    hg38 = MemmapGenome(
+        input_path=ORCA_PATH + "/resources/Homo_sapiens.GRCh38.dna.primary_assembly.fa",
+        memmapfile=ORCA_PATH + "/resources/Homo_sapiens.GRCh38.dna.primary_assembly.fa.mmap",
+        init_unpickleable=True,
+    )
---
+++ misc/process_expectation.py
@@ -0,0 +1,52 @@
+"""
+This script process the compute-expected output from cooltools, 
+combine expectations across chromosomes, apply smoothing 
+(note the smoothing settings may need to be adjusted
+for your data) and transform to format the Orca uses. It outputs
+three files, the cis-interaction expectation .npy file, the
+monotonically transformed cis-interaction expectation .mono.npy file 
+(only differs from the non-monotonically transformed in very high distances),
+and a trans-interaction expectation .trans.npy file (contains only a scalar).
+
+
+Example usage: python process_expectation.py expectation_file_res1000 1000
+
+"""
+import pandas as pd
+import sys
+import numpy as np
+
+res = int(sys.argv[2])
+
+expected = pd.read_csv(sys.argv[1], sep="\t")
+expectedsum = expected.groupby(["diag"]).agg({"n_valid": "sum", "balanced.sum": "sum"})
+expectedsum["balanced.avg"] = expectedsum["balanced.sum"] / expectedsum["n_valid"]
+expectedtrans = pd.read_csv(sys.argv[1] + ".trans", sep="\t")
+
+from statsmodels.nonparametric.smoothers_lowess import lowess
+
+v = np.log(expectedsum["balanced.avg"].values)
+v = v[: np.min(np.argwhere(~np.isfinite(v)))]
+sv0 = lowess(
+    v[int(400 / (res / 4000)) :], np.log(np.arange(int(400 / (res / 4000)), len(v)) + 1), frac=0.01
+)[:, 1]
+sv2 = lowess(
+    v[int(400 / (res / 4000)) :], np.log(np.arange(int(400 / (res / 4000)), len(v)) + 1), frac=0.1
+)[:, 1]
+sv = np.hstack(
+    [
+        v[: int(400 / (res / 4000))],
+        sv0[: int(10000 / (res / 4000))],
+        sv2[int(10000 / (res / 4000)) :],
+    ]
+)
+
+sv_mono = np.minimum.accumulate(sv)
+
+np.save(arr=sv, file=sys.argv[1] + ".npy")
+np.save(arr=sv_mono, file=sys.argv[1] + ".mono.npy")
+np.save(
+    arr=np.log(np.sum(expectedtrans["balanced.sum"]) / np.sum(expectedtrans["n_valid"])),
+    file=sys.argv[1] + ".trans.npy",
+)
+
